b948a76720948d9f5744f7ee8f5a9b086bf28f6e
ady254 <itsadnanahmad5@gmail.com>
2026-02-19 11:55:01 +0530

    Initial commit: Set up Vite project structure

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import RamadanTracker from './project.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <RamadanTracker />
+    </React.StrictMode>,
+)
